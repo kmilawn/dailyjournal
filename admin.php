65654313f24e8c902da7bd568c4c6ad8e8946566
kmilawn <najwahk354@gmail.com>
2025-01-11 00:25:17 +0700

    UAS WEB FINISH REAL

--- admin.php
+++ admin.php
@@ -71,7 +71,7 @@ if (!isset($_SESSION['username'])) {
               <ul class="dropdown-menu">
               <li class="nav-item">
             </li> 
-            <a class="nav-link" href="admin.php?page=update_profile">Profile</a>
+            <a class="nav-link" href="admin.php?page=profile">Profile</a>
                 <li><a class="dropdown-item" href="logout.php">Logout</a></li>
               </ul>
             </li>
